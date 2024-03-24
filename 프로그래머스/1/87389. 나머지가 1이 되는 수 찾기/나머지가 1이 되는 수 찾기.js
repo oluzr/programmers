6718fd246dd9120eeec68146b610cabd1f360d91
이나라 <94340943+oluzr@users.noreply.github.com>
2024-03-24 15:47:51 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,7 +1,5 @@
 function solution(n) {
-    for(let i = 0 ; i<=n ; i++)
-        if(n%i==1){
-            return i;
-            break;
-        }
+    for(x=2; x<n ; x++){
+        if(n%x === 1) return x
+    }
 }
