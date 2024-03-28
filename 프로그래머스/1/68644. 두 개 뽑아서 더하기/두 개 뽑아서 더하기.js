e2cb6f8a5e8b9afa9e7ea9d168d13f81fb52efe1
이나라 <94340943+oluzr@users.noreply.github.com>
2024-03-28 12:35:18 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.66 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = []
+    numbers.forEach((e,i)=>{
+        if(i !== numbers.length-1){
+            for(let j = i+1; j < numbers.length ; j++){
+                answer.push(e+numbers[j])
+            }
+        }
+    })
+    return answer.filter((e,i)=>answer.indexOf(e)===i).sort((a,b)=>a-b)
+}
