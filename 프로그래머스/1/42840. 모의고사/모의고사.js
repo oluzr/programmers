03725b70dc7d09fe3d361711eb7e319297dbdf94
이나라 <94340943+oluzr@users.noreply.github.com>
2024-04-01 18:28:03 +0900

    [level 1] Title: 모의고사, Time: 1.89 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,22 @@
+function solution(answers) {
+
+
+    const man1 = [1,2,3,4,5]
+    const man2 = [2,1,2,3,2,4,2,5]
+    const man3 = [3,3,1,1,2,2,4,4,5,5]
+    let max = 0; 
+    let checkFun = (man) => {
+        const length = answers.filter((e,i)=>(
+            e === man[i % man.length]
+        )).length
+        max = Math.max(max,length)
+        return length
+    }
+    const mans = {
+        1 : checkFun(man1),
+        2 : checkFun(man2),
+        3 : checkFun(man3)
+    }
+    return Object.keys(mans).filter((e)=>mans[e] === max).map((e)=>parseInt(e)).sort((a,b)=>a-b)
+    
+}
